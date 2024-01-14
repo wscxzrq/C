8d7f1f6555356d91572f5df7ccafe8caca7018a0
t-jie.xu3 <18330156357@163.com>
2024-01-15 00:49:45 +0800

    2024-01-15 C 语言学习

--- index.c
+++ index.c
@@ -1,6 +1,18 @@
-/* example1.1 The first C Program*/
 #include <stdio.h>
 int main() {
-  printf("This is a c program.\n");
-  return 0;
+  int max(int x, int y);
+  int a,b,c;
+  scanf("%d,%d",&a,&b);
+  c = max(a,b);
+  printf("max=%d",c);
+}
+
+int max(int x, int y) {
+  int z;
+  if(x > y) {
+    z = x;
+  }else {
+    z = y;
+  }
+  return(z);
 }
