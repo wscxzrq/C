1892fb25133057cdef9eb9cdf17a62c3527c9a71
t-jie.xu3 <18330156357@163.com>
2024-03-28 21:54:35 +0800

    2024/3/28 C 语言学习

--- index.c
+++ index.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include "string.h"
+int f(int a){
+  int b=4;
+	static int c=4;
+	b++; // 5
+	c++; // 5
+	return(a+b+c); // 14  15  16
+}
 int main() {
-  int a=2,b=5;
-  printf("%d\n",a%b);
+  int a=4,i;
+  for(i=0;i<3;i++) {
+    printf("%4d",f(a));
+  }
 
   return 0;
 }
+
